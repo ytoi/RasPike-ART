732d394c2c8394de20a9022057fab7c285ff960d
Yuki Tsuchitoi <y_toi@mac.com>
2024-06-30 16:28:27 +0900

    ヘッダを整理

--- include/raspike_protocol_api.h
+++ include/raspike_protocol_api.h
@@ -7,21 +7,10 @@
 extern "C" {
 #endif  
 
-  typedef unsigned char RasPikePort;
   extern int raspike_prot_init(RPComDescriptor *desc);
+  extern int raspike_prot_receive(void);
   extern int raspike_prot_shutdown(void);
 
-  
-  
-  extern int raspike_prot_lock_status(void);
-  extern int raspike_prot_unlock_status(void);
-
-  extern int raspike_prot_send(RasPikePort port, unsigned char cmdid, const unsigned char *buf, int size);
-  extern int raspike_prot_receive(void);
-  extern int raspike_wait_ack(unsigned char port, unsigned char cmd);
-
-  extern int raspike_wait_port_cmd_change(RasPikePort port, unsigned char wait_cmd);
-  extern int raspike_port_com_change_if_needed(RasPikePort port, unsigned char wait_cmd);
   
 #ifdef __cplusplus
 }
--- src/raspike_protocol_com.h
+++ src/raspike_protocol_com.h
@@ -176,6 +176,8 @@
 #define RASPIKE_PORT_DATA_SIZE (12)
 
 
+typedef unsigned char RasPikePort;
+
 
 typedef struct {
     RasPikePort port;
@@ -198,27 +200,6 @@ extern RPProtocolSpikeStatus *raspike_prot_get_saved_status(void);
 
 #define RP_PROTOCOL_BUFMAX (256)
 
-
-// Command Parameters
-typedef struct {
-  char direction;
-  char reset_count;
-} RPProtocolParamMotorSetup;
-
-// For set_speed,set_power
-typedef struct {
-  int val;
-} RPProtocolParamMotorValue;
-
-
-
-// Status parameters
-// Motor
-typedef struct {
-  int32_t count;
-  int32_t speed;
-  int32_t power;
-} RPProtocolStatusParamMotor;
 
 
 #endif // _RASPIKE_PROTOCOL_H
